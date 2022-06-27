a16a8377bd68ff34b7174e0cab5be63e94e6e099
黃士桓 <huan5678@gmail.com>
2022-06-28 01:44:16 +0800

    :construction: [update] 連接metaMask 取得錢包資訊

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,20 +1,46 @@
+import { useState } from 'react';
 import { ethers } from 'ethers';
 import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 
 function App() {
-  const provider = new ethers.providers.Web3Provider(window.ethereum);
-  const signer = provider.getSigner();
-  const nonce = uuidv4();
-  const metamaskSignin = async () => {
-    const signature = await signer.signMessage(nonce); // request user to sign the nonce and get the signature
-    console.log(signature);
+  const [walletAddress, setWalletAddress] = useState('');
+  const requestAccount = async () => {
+    if (window.ethereum) {
+      console.log('detected metamask');
+      try {
+        const account = await window.ethereum.request({
+          method: 'eth_requestAccounts',
+        });
+        setWalletAddress(account[0]);
+      } catch (err) {
+        console.log(err);
+      }
+    } else {
+      console.log('no metamask extension');
+      window.location.href = 'https://metamask.io/';
+    }
+  };
+  const connectWallet = async () => {
+    if (window.ethereum !== 'undefined') {
+      await requestAccount();
+
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const nonce = uuidv4();
+      const signature = await signer.signMessage(nonce); // request user to sign the nonce and get the signature
+      console.log(signature);
+    }
   };
   return (
     <div className="App">
-      <button type="button" className="" onClick={() => metamaskSignin}>
+      <button type="button" onClick={connectWallet}>
+        Connect Wallet
+      </button>
+      <button type="button" className="" onClick={requestAccount}>
         登入
       </button>
+      <p>{walletAddress}</p>
     </div>
   );
 }
